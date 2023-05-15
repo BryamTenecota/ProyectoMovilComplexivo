a259f28279cc0ef2e9a231dad204063afe77722a
CarlosI01 <91437492+CarlosI01@users.noreply.github.com>
2023-05-15 12:18:33 -0500

    Cambios realizados
    Cambios Realizados

---
+++ app/src/main/java/modelos/Roles.java
@@ -0,0 +1,23 @@
+package modelos;
+
+public class Roles {
+    private int idrol;
+    private String rolNombre;
+
+    public int getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(int idrol) {
+        this.idrol = idrol;
+    }
+
+    public String getRolnombre() {
+        return rolNombre;
+    }
+
+    public void setRolNombre(String rolNombre) {
+        this.rolNombre = rolNombre;
+    }
+
+}
--- app/src/main/java/modelos/Usuario.java
+++ app/src/main/java/modelos/Usuario.java
@@ -1,14 +1,69 @@
 package modelos;
 
+import java.util.List;
+
 public class Usuario {
 
-    private PersonaEmpresa personaEmpresa;
+    private int idusuario;
+    private String cedula;
+    private String correo;
+    private String contrasenia;
+    private String nombres;
+    //Roles
+    private List<Roles> roles;
+    //Atributo statico para traer usuarios
+    public static String rol;
+    //Encriptar contrasenia
+//    public static boolean verificarPassword(String passwordIngresada, String hashBCrypt) {
+//        return BCrypt.checkpw(passwordIngresada, hashBCrypt);
+//    }
 
-    public PersonaEmpresa getPersonaEmpresa() {
-        return personaEmpresa;
+
+    public int getIdusuario() {
+        return idusuario;
     }
 
-    public void setPersonaEmpresa(PersonaEmpresa personaEmpresa) {
-        this.personaEmpresa = personaEmpresa;
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public List<Roles> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(List<Roles> roles) {
+        this.roles = roles;
     }
 }
